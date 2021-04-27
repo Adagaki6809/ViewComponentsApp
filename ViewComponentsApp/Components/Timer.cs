0b8fdced8d9e4286deab6e2eb1ada4bb7bfa75ff
Hitagi <include.ofstream.h@gmail.com>
2021-04-27 17:33:53 +0300

    first commit

---
+++ ViewComponentsApp/Components/Timer.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace ViewComponentsApp.Components
+{
+    [ViewComponent]
+    public class Timer
+    {
+        public string Invoke(bool includeSeconds, bool format24)
+        {
+            
+            string time = String.Empty;
+            DateTime now = DateTime.Now;
+
+            if (format24)   // если 24-часовой формат
+                time = now.ToString("HH:mm");
+            else           // если 12-часовой формат
+                time = now.ToString("hh:mm");
+
+            if (includeSeconds) // если надо добавить секунды
+                time = $"{time}:{now.Second}";
+
+            return $"Tекущее время: {time}";
+        }
+    }
+}
